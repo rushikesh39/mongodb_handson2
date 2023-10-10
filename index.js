ba66cf29a598662db1fc724c72353643f01a887e
rushi <rushikeshthange2001@gmail.com>
2023-10-10 08:36:28 +0530

    first commit

--- index.js
+++ index.js
@@ -13,9 +13,20 @@ async function dbconnect(){
         console.log(e)
     }
 }
+
+app.get('/',(req,res)=>{
+  res.send({
+    1:'/insert',
+    2:'/finduser30000',
+    3:'/finduser_exp2',
+    4:"/findUserwith_yearGrad_after2015_and_exp_greaterthan1",
+    5:'/update'
+  })
+})
 async function startServer() {
     try {
       const db = await dbconnect();
+     
       app.use("/",userRoutes)
       app.listen(5000, () => console.log('Server is running on port 5000'));
     } catch (error) {
