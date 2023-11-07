d4bbdca6d05c93fa39e290840938a13556015589
rushi <rushikeshthange2001@gmail.com>
2023-11-07 19:12:31 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const route = require("./routes/emp_routes")
 const {connect} = require("./db/connection")
 const app=express()
 app.use(express.json())
+app.get("/",(req,res)=>{
+res.send({
+  1:"/insert",
+  2:"/salary_gt_30k",
+  3:"/exp_gt_2",
+  4:"/yearGrad_gt_2015_exp_gt_2",
+  5:"/updateSalary"
+})
+})
 app.use("/api",route)
 app.listen(5000,async()=>{
   try{
